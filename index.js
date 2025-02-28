efcc0f1562306193f1fd5d4076fec9775c1c976d
KatochAarushi <katochaarushi@gmail.com>
2025-02-28 14:11:16 -0500

    Video

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Aarushi was here!');
+    res.send('Aarushi Katoch was here!');
 });
 
 app.listen(port, () => {
