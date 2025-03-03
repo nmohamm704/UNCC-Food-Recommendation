6aaecd47443c090ef4e2cf13b9140227dd3e9e48
Naveed Mohammad <nmohamm7@uncc.edu>
2025-03-03 17:10:33 -0500

    map attribution

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var map = L.map("map").setView([35.3076, -80.7336], 14);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "&copy; OpenStreetMap contributors"
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 });
