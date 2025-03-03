a0b6f83f77c307530385d2871ca3dd3391d35f17
Naveed Mohammad <nmohamm7@uncc.edu>
2025-03-03 16:57:08 -0500

    map api implementation

---
+++ frontend/js/map.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var map = L.map("map").setView([35.3076, -80.7336], 14);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors"
+    }).addTo(map);
+});
