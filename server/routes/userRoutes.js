581450393f1aaa05f0ade98a045d8fa1ec1e2f41
vKrystic <nvolinsk@uncc.edu>
2025-04-07 16:17:37 -0400

    Added more functionality to Profile, Home, and Favorites pages. Small changes to HTML and JS files

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -14,13 +14,22 @@ router.post('/login', userController.login);
 router.put("/profile", authenticate, upload.single('profileImage'), userController.update);
 
 // Protected Route Example (for getting user profile)
-/* router.get('/profile', authenticate, async (req, res) => {
+router.get('/profile', authenticate, async (req, res) => {
     try {
-        res.json({ message: `Welcome, User ${req.user.userId}` });
+        const user = await require('../models/userModel').findById(req.user.userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.json({
+            name: user.name,
+            email: user.email,
+            profileImage: user.profileImage
+        });
     } catch (error) {
         res.status(500).json({ message: 'Failed to fetch profile', error });
     }
-}); */
+});
 
 // Get All Favorites
 router.get('/favorites', authenticate, userController.getFavoriteRestaurants);
